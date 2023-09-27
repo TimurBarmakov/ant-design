c8768039de1a86aac4989a8fd511268d405fb516
Timur Barmakov <tbarmakov@bk.ru>
2023-09-27 13:11:41 +0600

    authorization fix 15

--- mock/listTableList.ts
+++ mock/listTableList.ts
@@ -33,78 +33,78 @@ const genList = (current: number, pageSize: number) => {
 
 let tableListDataSource = genList(1, 100);
 
-function getRule(req: Request, res: Response, u: string) {
-  let realUrl = u;
-  if (!realUrl || Object.prototype.toString.call(realUrl) !== '[object String]') {
-    realUrl = req.url;
-  }
-  const { current = 1, pageSize = 10 } = req.query;
-  const params = parse(realUrl, true).query as unknown as API.PageParams &
-    API.RuleListItem & {
-      sorter: any;
-      filter: any;
-    };
+// function getRule(req: Request, res: Response, u: string) {
+//   let realUrl = u;
+//   if (!realUrl || Object.prototype.toString.call(realUrl) !== '[object String]') {
+//     realUrl = req.url;
+//   }
+//   const { current = 1, pageSize = 10 } = req.query;
+//   const params = parse(realUrl, true).query as unknown as API.PageParams &
+//     API.RuleListItem & {
+//       sorter: any;
+//       filter: any;
+//     };
 
-  let dataSource = [...tableListDataSource].slice(
-    ((current as number) - 1) * (pageSize as number),
-    (current as number) * (pageSize as number),
-  );
-  if (params.sorter) {
-    const sorter = JSON.parse(params.sorter);
-    dataSource = dataSource.sort((prev, next) => {
-      let sortNumber = 0;
-      (Object.keys(sorter) as Array<keyof API.RuleListItem>).forEach((key) => {
-        let nextSort = next?.[key] as number;
-        let preSort = prev?.[key] as number;
-        if (sorter[key] === 'descend') {
-          if (preSort - nextSort > 0) {
-            sortNumber += -1;
-          } else {
-            sortNumber += 1;
-          }
-          return;
-        }
-        if (preSort - nextSort > 0) {
-          sortNumber += 1;
-        } else {
-          sortNumber += -1;
-        }
-      });
-      return sortNumber;
-    });
-  }
-  if (params.filter) {
-    const filter = JSON.parse(params.filter as any) as {
-      [key: string]: string[];
-    };
-    if (Object.keys(filter).length > 0) {
-      dataSource = dataSource.filter((item) => {
-        return (Object.keys(filter) as Array<keyof API.RuleListItem>).some((key) => {
-          if (!filter[key]) {
-            return true;
-          }
-          if (filter[key].includes(`${item[key]}`)) {
-            return true;
-          }
-          return false;
-        });
-      });
-    }
-  }
+//   let dataSource = [...tableListDataSource].slice(
+//     ((current as number) - 1) * (pageSize as number),
+//     (current as number) * (pageSize as number),
+//   );
+//   if (params.sorter) {
+//     const sorter = JSON.parse(params.sorter);
+//     dataSource = dataSource.sort((prev, next) => {
+//       let sortNumber = 0;
+//       (Object.keys(sorter) as Array<keyof API.RuleListItem>).forEach((key) => {
+//         let nextSort = next?.[key] as number;
+//         let preSort = prev?.[key] as number;
+//         if (sorter[key] === 'descend') {
+//           if (preSort - nextSort > 0) {
+//             sortNumber += -1;
+//           } else {
+//             sortNumber += 1;
+//           }
+//           return;
+//         }
+//         if (preSort - nextSort > 0) {
+//           sortNumber += 1;
+//         } else {
+//           sortNumber += -1;
+//         }
+//       });
+//       return sortNumber;
+//     });
+//   }
+//   if (params.filter) {
+//     const filter = JSON.parse(params.filter as any) as {
+//       [key: string]: string[];
+//     };
+//     if (Object.keys(filter).length > 0) {
+//       dataSource = dataSource.filter((item) => {
+//         return (Object.keys(filter) as Array<keyof API.RuleListItem>).some((key) => {
+//           if (!filter[key]) {
+//             return true;
+//           }
+//           if (filter[key].includes(`${item[key]}`)) {
+//             return true;
+//           }
+//           return false;
+//         });
+//       });
+//     }
+//   }
 
-  if (params.name) {
-    dataSource = dataSource.filter((data) => data?.name?.includes(params.name || ''));
-  }
-  const result = {
-    data: dataSource,
-    total: tableListDataSource.length,
-    success: true,
-    pageSize,
-    current: parseInt(`${params.current}`, 10) || 1,
-  };
+//   if (params.name) {
+//     dataSource = dataSource.filter((data) => data?.name?.includes(params.name || ''));
+//   }
+//   const result = {
+//     data: dataSource,
+//     total: tableListDataSource.length,
+//     success: true,
+//     pageSize,
+//     current: parseInt(`${params.current}`, 10) || 1,
+//   };
 
-  return res.json(result);
-}
+//   return res.json(result);
+// }
 
 function postRule(req: Request, res: Response, u: string, b: Request) {
   let realUrl = u;
@@ -172,6 +172,6 @@ function postRule(req: Request, res: Response, u: string, b: Request) {
 }
 
 export default {
-  'GET /api/rule': getRule,
+  // 'GET /api/rule': getRule,
   'POST /api/rule': postRule,
 };
